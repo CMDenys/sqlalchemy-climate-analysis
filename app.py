4138f3a3691b983a311950bb6962fd119cd42c40
Charlie Denys <charlie@Charlies-MBP.hwccustomers.com>
2021-03-01 23:20:29 -0500

    completed climate analysis and app.py

--- app.py
+++ app.py
@@ -40,9 +40,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/temperature/<start>/<end>"
-    )
-
+        "/api/v1.0/{start}/{end}"
+        )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -64,12 +63,42 @@ def stations():
     session = Session(engine)
     #Perform query to retrieve station data
     results = session.query(Station.name).all()
-
     all_stations = list(np.ravel(results))
+    session.close()
     return jsonify(all_stations)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    #create session link from Python to the DB
+    session = Session(engine)
+    #Define date parameters
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    #Perform query to retrieve temperature data
+    results = session.query(Measurement.tobs).\
+    filter(Measurement.date > year_ago).\
+    filter(Measurement.station == 'USC00519281').all()
 
+    active_station_data = list(np.ravel(results))
+    session.close()
+    return jsonify(active_station_data)
 
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    #create session link from Python to the DB
+    session = Session(engine)
+
+    min_temp = func.min(Measurement.tobs)
+    avg_temp = func.avg(Measurement.tobs)
+    max_temp = func.max(Measurement.tobs)
+
+    temperature_results = session.query(min_temp, avg_temp, max_temp).\
+    filter(Measurement.date >= start).\
+    filter(Measurement.date <= end).all()
+
+    # temperature_results = list(np.ravel)
+
+    session.close
+    return jsonify(temperature_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
